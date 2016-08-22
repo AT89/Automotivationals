843a4c83f1b72e79e51aac44497ff3805bebd778
Anh Truong <anh.truong.07@cnu.edu>
2016-08-22 15:47:34 -0400

    cleaned up some code

--- ST.js
+++ ST.js
@@ -1,54 +1,54 @@
 var load = function()
 {
 	$.ajax(
-	{
-		url: 'https://www.reddit.com/r/EarthPorn.json',
-		dataType: 'json',
-		success: function(data)
 		{
-			var post = null;
-			while(post == null || post.data.title.match(/earth ?porn/i) || post.data.title.match(/poll/i))
+			url: 'https://www.reddit.com/r/EarthPorn.json',
+			dataType: 'json',
+			success: function(data)
 			{
-				post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
+				var post = null;
+				while(post == null || post.data.title.match(/earth ?porn/i) || post.data.title.match(/poll/i))
+				{
+					post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
+				}
+				if(post.data.preview.images[0] && post.data.preview.images[0].source)
+				{
+					$('body').css('background-image', 'url(' + post.data.preview.images[0].source.url + ')');
+				}
 			}
-			if(post.data.preview.images[0] && post.data.preview.images[0].source)
-			{
-				$('body').css('background-image', 'url(' + post.data.preview.images[0].source.url + ')');
-			}
-		}
-	});
-
-	$.ajax(
-	{
-		url: 'https://www.reddit.com/r/ShowerThoughts.json',
-		dataType: 'json',
-		success: function(data)
-		{
-			var post = null,
-				title = $('h1');
-			while(post == null || post.data.title.match(/Shower? thoughsts/i))
-			{
-				post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
-			}
-//changing URL here
-			url = $('#reddit_url').attr("href", post.data.url)
-    	title.text(post.data.title.trim().replace(/\.$/, ''))
-			$(window).trigger('resize');
-		}
-	});
-};
-
-//refresh page on click anywhere
-$(function()
-{
-	load();
-	$('body:not(.rbutton)').on('click', load); //WHY U NOT WORKING
-	$(window).on('resize scroll', function()
-	{
-		var title = $('h1');
-		title.css(
-		{
-			'margin': '-' + (title.height() / 2) + 'px 0 0'
 		});
-	});
-});
+
+		$.ajax(
+			{
+				url: 'https://www.reddit.com/r/ShowerThoughts.json',
+				dataType: 'json',
+				success: function(data)
+				{
+					var post = null,
+					title = $('h1');
+					while(post == null || post.data.title.match(/Shower? thoughsts/i))
+					{
+						post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
+					}
+					//changing URL here
+					url = $('#reddit_url').attr("href", post.data.url)
+					title.text(post.data.title.trim().replace(/\.$/, ''))
+					$(window).trigger('resize');
+				}
+			});
+		};
+
+		//refresh page on click anywhere
+		$(function()
+		{
+			load();
+			$('body:not(.rbutton)').on('click', load); //WHY U NOT WORKING
+			$(window).on('resize scroll', function()
+			{
+				var title = $('h1');
+				title.css(
+					{
+						'margin': '-' + (title.height() / 2) + 'px 0 0'
+					});
+				});
+			});
--- TIFU.js
+++ TIFU.js
@@ -1,69 +1,57 @@
 var load = function()
 {
 	$.ajax(
-	{
-		url: 'https://www.reddit.com/r/EarthPorn.json',
-		dataType: 'json',
-		success: function(data)
 		{
-			var post = null;
-
-			while(post == null || post.data.title.match(/earth ?porn/i) || post.data.title.match(/poll/i))
+			url: 'https://www.reddit.com/r/EarthPorn.json',
+			dataType: 'json',
+			success: function(data)
 			{
-				post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
+				var post = null;
+				while(post == null || post.data.title.match(/earth ?porn/i) || post.data.title.match(/poll/i))
+				{
+					post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
+				}
+				if(post.data.preview.images[0] && post.data.preview.images[0].source)
+				{
+					$('body').css('background-image', 'url(' + post.data.preview.images[0].source.url + ')');
+				}
 			}
-
-			if(post.data.preview.images[0] && post.data.preview.images[0].source)
-			{
-				$('body').css('background-image', 'url(' + post.data.preview.images[0].source.url + ')');
-			}
-		}
-	});
-
-	$.ajax(
-	{
-		url: 'https://www.reddit.com/r/tifu.json',
-		dataType: 'json',
-		success: function(data)
-		{
-			var post = null,
-				title = $('h1'),
-				contents = $('#TIFUcontents')
-				//add URL link
-
-			while((post == null || post.data.title.match(/today ? I ? fucked ? up/i))&& post == null || post.data.selftext.match(/today ? I ? fucked ? up/i) ) //not working :(
-			{
-				post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
-
-//SLICE(0,4) WHY U NOT WORK
-
-
-//changing URL here
-			url = $('#reddit_url').attr("href", post.data.url)
-			}
-
-			title.text(post.data.title.trim().replace(/\.$/, ''))
-
-
-			$(window).trigger('resize');
-		}
-	});
-};
-
-$(function()
-{
-	load();
-
-	$('body').on('click', load);
-
-	$(window).on('resize scroll', function()
-	{
-		var title = $('h1');
-		var contents = $('#TIFUcontents')
-
-		title.css(
-		{
-			'margin': '-' + (title.height() / 2) + 'px 0 0'
 		});
-	});
-});
+
+		$.ajax(
+			{
+				url: 'https://www.reddit.com/r/tifu.json',
+				dataType: 'json',
+				success: function(data)
+				{
+					var post = null,
+					title = $('h1'),
+					contents = $('#TIFUcontents')
+					//add URL link
+					while((post == null || post.data.title.match(/today ? I ? fucked ? up/i))&& post == null || post.data.selftext.match(/today ? I ? fucked ? up/i) ) //not working :(
+						{
+							post = data.data.children[Math.floor(Math.random() * data.data.children.length)];
+							//SLICE(0,4) WHY U NOT WORK
+							//changing URL here
+							url = $('#reddit_url').attr("href", post.data.url)
+						}
+						title.text(post.data.title.trim().replace(/\.$/, ''))
+						$(window).trigger('resize');
+					}
+				});
+			};
+
+			$(function()
+			{
+				load();
+				$('body').on('click', load);
+				$(window).on('resize scroll', function()
+				{
+					var title = $('h1');
+					var contents = $('#TIFUcontents')
+					title.css(
+						{
+							'margin': '-' + (title.height() / 2) + 'px 0 0'
+						});
+					});
+				});
